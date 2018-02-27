07d1b3a8898b5a5e6ceebbe76d3bf5ad6106177b
houdunwang <2300071698@qq.com>
2018-02-27 14:27:42 +0800

    常规优化

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -20,9 +20,16 @@ class ConfigProvider extends Provider
     public function boot()
     {
         //加载.env文件并加载配置文件
-        Config::env('.env')->loadFiles(ROOT_PATH.'/system/config');
+        Config::env('.env')->loadFiles(ROOT_PATH . '/system/config');
+
         //设置时区
         date_default_timezone_set(Config::get('app.timezone'));
+
+        //调试时允许跨域访问
+        if (Config::get('app.debug')) {
+            header('Access-Control-Allow-Origin:*');
+            header('Access-Control-Allow-Headers:*');
+        }
     }
 
     public function register()
